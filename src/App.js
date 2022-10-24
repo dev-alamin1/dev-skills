2ecd2ea228b04563d361fae7f1edfe67f459218c
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-24 21:32:41 +0600

    Create separate router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Router';
 
 function App() {
+  
   return (
-    <div className="App h-screen flex justify-center items-center">
-      <h2 className='text-5xl text-center'>Hello ,<br /> <span className='text-orange-400'>React Js</span> Lovers </h2>
-    </div>
+    <RouterProvider  router={router}/>
   );
 }
 
---
+++ src/Router/Router.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+/*
+|---------------------------
+| Website Router And Element 
+|---------------------------
+*/
+const router = createBrowserRouter([
+
+    {
+      path:'/',
+      element:<div>Homepage</div>
+    }
+  ]);
+
+ export default router;
