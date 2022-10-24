3dfb528b0ba6f8050bddd143cdbb224fae36098e
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-24 21:05:03 +0600

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex justify-center items-center">
+      <h2 className='text-5xl text-center'>Hello ,<br /> <span className='text-orange-400'>React Js</span> Lovers </h2>
     </div>
   );
 }
