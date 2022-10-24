5eaf2deed1329d1013aae37c0b954eaba019b434
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-24 23:06:29 +0600

    Navbar Logo, menu added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,36 +1,81 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className='px-10 md:px-20'>
-              <div className="navbar bg-base-100">
+        <div>
+             <div className="navbar bg-base-300 px-5 md:px-20">
+                    {/* 
+                    |------------------------------
+                    | Navbar Start (Left Side)
+                    |-----------------------------
+                    */}
                     <div className="navbar-start">
                         <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Homepage</a></li>
-                            <li><a>Portfolio</a></li>
-                            <li><a>About</a></li>
-                        </ul>
-                        </div>
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
+                             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-auto">
+                                <li><Link>Home</Link></li>
+                                <li><Link>Courses</Link></li>
+                                <li><Link>Blog</Link></li>
+                                <li><Link>FaQ</Link></li>
+                             </ul>
+                       </div>
+                     <div className='hidden md:flex items-center gap-1'>
+                         <img src="dev-logo.png" alt="" className='w-12 h-10'/>
+                         <Link className="text-2xl font-bold text-orange-400">Skill</Link>
                     </div>
-                    <div className="navbar-center">
-                        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                    </div>
-                    <div className="navbar-end">
-                        <button className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                        </button>
-                        <button className="btn btn-ghost btn-circle">
-                        <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                            <span className="badge badge-xs badge-primary indicator-item"></span>
-                        </div>
-                        </button>
+               </div>
+ 
+
+                {/* 
+                |------------------------------
+                | Navbar Center 
+                |-----------------------------
+                */}
+
+               <div className="navbar-center">
+                   <div className='flex items-center gap-1 md:hidden ml-10'>
+                         <img src="dev-logo.png" alt="" className='w-12 h-10'/>
+                         <Link className=" text-2xl font-bold text-orange-400">Skill</Link>
                     </div>
                 </div>
+
+                {/* 
+                |------------------------------
+                | Navbar End (Right Side)
+                |-----------------------------
+                */}
+
+                <div className="navbar-end mr-4">
+                        <ul className="menu menu-horizontal px-2 hidden md:flex">
+                                <li><Link>Home</Link></li>
+                                <li><Link>Courses</Link></li>
+                                <li><Link>Blog</Link></li>
+                                <li><Link>FaQ</Link></li>
+                       </ul>
+                </div>
+
+                {/* 
+                |------------------------------------------
+                | Navbar End (Profile And Profile Dropdown)
+                |------------------------------------------
+                */}
+
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="https://placeimg.com/80/80/people" alt='' />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                           <Link className="justify-between"> Profile<span className="badge">New</span></Link>
+                        </li>
+                        <li><Link>Logout</Link></li>
+                    </ul>
+                </div>
+
+             </div>
         </div>
     );
 };
